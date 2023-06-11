0932a6c30986a2d8c6d8c2c5d8d535237ce44ac9
Abhiroop <asiamgenius@gmail.com>
2023-06-11 02:31:58 +0100

    Add some induction proofs

--- LF/Induction.v
+++ LF/Induction.v
@@ -1,1 +1,34 @@
 From LF Require Export Basics.
+
+Theorem add_0_r : forall n:nat, n + 0 = n.
+Proof.
+intros n. induction n  as [| n' IHn'].
+- reflexivity.
+- simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem minus_n_n : forall n,
+  minus n n = 0.
+Proof.
+intros n. induction n as [|n' IHn'].
+- reflexivity.
+- simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Theorem mul_0_r : forall n:nat,
+  n * 0 = 0.
+Proof.
+induction n.
+- reflexivity.
+- simpl. rewrite IHn. reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat,
+  S (n + m) = n + (S m).
+Proof.
+intros n.
+intros m.
+induction n.
+- reflexivity.
+- simpl. rewrite IHn. reflexivity.
+Qed.
