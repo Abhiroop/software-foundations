04da010fc4e0ae9db9256e020c324f30fc39e63c
Abhiroop <asiamgenius@gmail.com>
2024-07-17 18:27:39 +0200

    Playing with products

--- LF/Lists.v
+++ LF/Lists.v
@@ -1,1 +1,67 @@
 From LF Require Export Induction.
+
+Module NatList.
+
+Inductive natprod : Type :=
+  | pair (n1 n2 : nat).
+
+Check (pair 3 5) : natprod.
+
+Definition fst (p : natprod) : nat :=
+  match p with
+  | pair x y => x
+  end.
+
+Definition snd (p : natprod) : nat :=
+  match p with
+  | pair x y => y
+  end.
+
+Compute (fst (pair 3 5)).
+
+Notation "( x , y )" := (pair x y).
+
+Compute (fst (3,5)).
+
+Definition fst' (p : natprod) : nat :=
+  match p with
+  | (x,y) => x
+  end.
+Definition snd' (p : natprod) : nat :=
+  match p with
+  | (x,y) => y
+  end.
+Definition swap_pair (p : natprod) : natprod :=
+  match p with
+  | (x,y) => (y,x)
+  end.
+
+Theorem surjective_pairing' : forall (n m : nat),
+  (n,m) = (fst (n,m), snd (n,m)).
+Proof. reflexivity. Qed.
+
+Theorem surjective_pairing : forall (p : natprod),
+  p = (fst p, snd p).
+Proof.
+intros.
+destruct p.
+reflexivity.
+Qed.
+
+Theorem snd_fst_is_swap : forall (p : natprod),
+  (snd p, fst p) = swap_pair p.
+Proof.
+intros []. (* shorthand for intros p. destruct p. *)
+simpl.
+reflexivity.
+Qed.
+
+Theorem fst_swap_is_snd : forall (p : natprod),
+  fst (swap_pair p) = snd p.
+Proof.
+intros [].
+simpl.
+reflexivity.
+Qed.
+
+End NatList.
